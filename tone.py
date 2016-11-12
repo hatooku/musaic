e23b310f864fa0b895f6b0dcd6a8ccfec97f7135
hatooku <jhlee@caltech.edu>
2016-11-12 10:26:24 -0800

    Parallel API calls implemented for IBM Watson

--- tone.py
+++ tone.py
@@ -1,5 +1,7 @@
 import json
+import time
 from watson_developer_cloud import ToneAnalyzerV3
+from multiprocessing import Pool, Queue, Process
 
 
 tone_analyzer = ToneAnalyzerV3(
@@ -11,7 +13,7 @@ def get_emotions(lyrics):
     """Returns the emotions contained in the lyrics.
 
     Args:
-        lyrics (str): lyrics to be analyzed
+        lyrics (tuple): the lyrics to be analyzed
 
     Returns:
         data (dict): Valid keys are 'anger', 'joy', 'fear', 'sadness', 'disgust'
@@ -26,3 +28,33 @@ def get_emotions(lyrics):
         data[str(tone_data['tone_id'])] = tone_data['score']
         
     return data
+
+def get_all_emotions(songs_arr):
+    """Computes the emotions for the given array of songs in parallel.
+
+    Args:
+        songs_arr (arr): Array of songs in the form (id, lyrics)
+
+    Returns:
+        data (arr): Array of song data in the form (id, emotions) where
+            emotions is the dict defined in get_emotions.
+    """
+    def analyze_lyrics(queue, song):
+        queue.put((song[0], get_emotions(song[1])))
+    queue = Queue()
+    processes = []
+    for song in songs_arr:
+        p = Process(target=analyze_lyrics, args=(queue, song))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+    data = []
+
+    while not queue.empty():
+        data.append(queue.get())
+
+    return data
+
