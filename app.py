281b35d5d1eaecad4b9989a9f2e35a2f87412894
Richard Zhu <rzhu.96@gmail.com>
2016-11-12 18:03:42 -0800

    hello

--- app.py
+++ app.py
@@ -237,11 +237,11 @@ def logic():
 @app.route('/result',  methods=['GET', 'POST'])
 def result():
     form = PlaylistButton(request.form)
-    result_tracks = session['result_tracks']
+    # result_tracks = session['result_tracks']
 
     # get names and artists of those songs -> result_info
-    for track in result_tracks['tracks']:
-        result_info.append((track['name'], track['artists'][0]['name']))
+    # for track in result_tracks['tracks']:
+    #     result_info.append((track['name'], track['artists'][0]['name']))
     # print result_info later???
 
     if form.is_submitted():
