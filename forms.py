0c01e594bb69b0b71977c2b9c5f42de106573aeb
Jdonerme <jdonerme@caltech.edu>
2016-11-12 16:58:43 -0800

    fixed forms

--- forms.py
+++ forms.py
@@ -3,16 +3,13 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class MoodButtons(Form):
-    name = StringField('name', validators=[DataRequired()])
+    anger = SubmitField('Angry')
+    joy = SubmitField('Hapy')
+    sadness = SubmitField('Sad')
 
-# class MoodButtons(Form):
-#     anger = SubmitField('Angry')
-#     joy = SubmitField('Hapy')
-#     sadness = SubmitField('Sad')
+class MoodText(Form):
+    text = StringField('Mood')
+    startButton = SubmitField("That's how I'm feeling baby")
 
-# class MoodText(Form):
-#     test = StringField('Username')
-#     startButton = SubmitField("That's how I'm feeling baby")
-
-#class PlaylistButton(Form):
-#	make = SubmitField("That sounds dank. I'll order 1 playlist of that please")
+class PlaylistButton(Form):
+	make = SubmitField("That sounds dank. I'll order 1 playlist of that please")
